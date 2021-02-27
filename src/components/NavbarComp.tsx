d3b066c0b96789bb6d7fb3d13cc2750a1dea5262
Gabriel Camurça <gabriel-camurca@users.noreply.github.com>
2021-02-27 20:54:23 -0300

    Home is now responsive

--- src/components/NavbarComp.tsx
+++ src/components/NavbarComp.tsx
@@ -8,8 +8,7 @@ export default function NavbarComp(){
         <div>
             <Navbar className={styles.outerNavBarContainer}>
                 <NavbarBrand href="/" className={styles.brandName}>GitHub Jobs</NavbarBrand>
-                {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-                <Navbar.Collapse className={styles.navBarContainer}>
+                <Navbar.Collapse className={styles.navBarContainer} >
                     <div className={styles.navItems}>
                         <Nav.Link href="/" className={styles.navSingleItem}>All jobs</Nav.Link>
                         <Nav.Link href="/howitworks" className={styles.navSingleItem}>How It Works</Nav.Link>
@@ -17,6 +16,7 @@ export default function NavbarComp(){
                     </div>
                 </Navbar.Collapse>
             </Navbar>
+
         </div>
     )
 }
