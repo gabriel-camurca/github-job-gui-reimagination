fc27f2de09b7b97d6b72b3bc98eb94c5bba12563
Gabriel Camurça <gabriel-camurca@users.noreply.github.com>
2021-02-28 13:04:35 -0300

    Minor details added

--- src/components/JobsList.tsx
+++ src/components/JobsList.tsx
@@ -1,6 +1,5 @@
 import { useContext, useState } from "react";
 import { JobCard } from "./JobCard";
-import styles from "../styles/components/JobsList.module.css"
 
 import  allJobs from "../../jobs.json"
 import axios from "axios";
@@ -12,21 +11,22 @@ export function JobsList(){
 
     const [jobs, setJobs] = useState(allJobs);
 
-    const [jobsFetched, setJobsFetched] = useState([]);
-
-    // https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json
-    // , {params: {markdown:true}}
-    // axios.get('https://jobs.github.com/positions.json',
-    // {   params:{ markdown: true},
-    //     headers: {'Access-Control-Allow-Origin' : '*', 'Accept' : 'text/plain'}
-    // }).then(res => console.log(res.data)).catch(e => {console.log(e)});
-
-    const [test, setTest] = useState([]);
 
     axios.get('https://jobs.github.com/positions.json',
-    {
+    {   params: {markdown: true, location: loc, description: desc, full_time: isChecked},
         headers: {'Access-Control-Allow-Origin' : '*', 'Accept' : 'text/plain'}
     }).then(res => setJobs(res.data)).catch(e => {console.log(e)})
+
+    // return(
+    //     <div style={{display:"flex",flexDirection:"column" ,justifyContent: "space-evenly", alignItems: "center" }}>
+    //         {jobs.map(item => {
+    //           if(String(item.description).toLowerCase().includes(desc) && String(item.location).toLowerCase().includes(loc)){
+    //             return <JobCard key={item.id} job={item} />
+    //             }
+    //           })
+    //         }
+    //     </div>
+    // );
 
     return(
         <div style={{display:"flex",flexDirection:"column" ,justifyContent: "space-evenly", alignItems: "center" }}>
